5fde802ff1a292614174d6825d78407c9c8d0b5f
Christof Thalmann <christof.thalmann@lingvano.com>
2023-02-22 10:58:27 +0100

    add java runtime

--- scripts/eas-build-on-success.sh
+++ scripts/eas-build-on-success.sh
@@ -5,6 +5,11 @@ set -eox pipefail
 curl -Ls "https://get.maestro.mobile.dev" | bash
 export PATH="$PATH":"$HOME/.maestro/bin"
 
+brew install java
+echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
+
 MAESTRO_API_KEY=process.env.MAESTRO_API_KEY
 APP_EXECUTABLE_PATH=/Users/expo/workingdir/build/ios/build/Build/Products/Release-iphonesimulator/ReactNativeEASMaestro.app
 
