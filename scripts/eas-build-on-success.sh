9fd2f46bbf138c4421f68ad5bd6ba98ed5979804
Christof Thalmann <christof.thalmann@lingvano.com>
2023-02-22 11:56:34 +0100

    add android support and update ios for eas-build-on-success

--- scripts/eas-build-on-success.sh
+++ scripts/eas-build-on-success.sh
@@ -10,7 +10,19 @@ echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
 sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
 export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
 
-MAESTRO_API_KEY=process.env.MAESTRO_API_KEY
-APP_EXECUTABLE_PATH=/Users/expo/workingdir/build/ios/build/Build/Products/Release-iphonesimulator/ReactNativeEASMaestro.app
+MAESTRO_API_KEY=$MAESTRO_API_KEY
+
+if [ "$EAS_BUILD_PLATFORM" = "ios" ]
+then
+   APP_EXECUTABLE_PATH=/Users/expo/workingdir/build/ios/build/Build/Products/Release-iphonesimulator/ReactNativeEASMaestro.app
+else
+   APK_PATH=$(find /Users/expo/workingdir/build -name '*.apk')
+   if [ -n "$APK_PATH" ]; then
+    APP_EXECUTABLE_PATH="$APK_PATH"
+  else
+    echo "Error: APK file not found"
+    exit 1
+  fi
+fi
 
 maestro cloud --apiKey $MAESTRO_API_KEY $APP_EXECUTABLE_PATH maestro/test.yaml
