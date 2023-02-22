28a711a0a5b9005db467cba12b87cb56bdc70466
Christof Thalmann <christof.thalmann@lingvano.com>
2023-02-22 13:11:13 +0100

    update android workflow

--- scripts/eas-build-on-success.sh
+++ scripts/eas-build-on-success.sh
@@ -5,15 +5,15 @@ set -eox pipefail
 curl -Ls "https://get.maestro.mobile.dev" | bash
 export PATH="$PATH":"$HOME/.maestro/bin"
 
-brew install java
-echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
-sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
-export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
-
 MAESTRO_API_KEY=$MAESTRO_API_KEY
 
 if [ "$EAS_BUILD_PLATFORM" = "ios" ]
 then
+   brew install java
+   echo 'export PATH="/opt/homebrew/opt/openjdk/bin:$PATH"' >> ~/.zshrc
+   sudo ln -sfn /opt/homebrew/opt/openjdk/libexec/openjdk.jdk /Library/Java/JavaVirtualMachines/openjdk.jdk
+   export CPPFLAGS="-I/opt/homebrew/opt/openjdk/include"
+
    APP_EXECUTABLE_PATH=/Users/expo/workingdir/build/ios/build/Build/Products/Release-iphonesimulator/ReactNativeEASMaestro.app
 else
    APK_PATH=$(find /Users/expo/workingdir/build -name '*.apk')
@@ -25,4 +25,4 @@ else
   fi
 fi
 
-maestro cloud --apiKey $MAESTRO_API_KEY $APP_EXECUTABLE_PATH maestro/test.yaml
+maestro cloud --apiKey $MAESTRO_API_KEY $APP_EXECUTABLE_PATH maestro/test.yaml  
