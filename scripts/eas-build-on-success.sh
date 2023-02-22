562d872f6e0228791860c7ffce19806263eebb99
Christof Thalmann <christof.thalmann@lingvano.com>
2023-02-22 10:50:43 +0100

    simplify workflow and always build on main push

--- scripts/eas-build-on-success.sh
+++ scripts/eas-build-on-success.sh
@@ -6,6 +6,6 @@ curl -Ls "https://get.maestro.mobile.dev" | bash
 export PATH="$PATH":"$HOME/.maestro/bin"
 
 MAESTRO_API_KEY=process.env.MAESTRO_API_KEY
-APP_EXECUTABLE_PATH=/Users/expo/workingdir/build/ios/build/Build/Products/Release-iphonesimulator/reactNativeEasMaestro.app
+APP_EXECUTABLE_PATH=/Users/expo/workingdir/build/ios/build/Build/Products/Release-iphonesimulator/ReactNativeEASMaestro.app
 
 maestro cloud --apiKey $MAESTRO_API_KEY $APP_EXECUTABLE_PATH .maestro/**/*.yml
