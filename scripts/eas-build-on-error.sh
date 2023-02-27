4faf6e83860d83222ad0ed209271c0155a073827
Christof Thalmann <christof.thalmann@lingvano.com>
2023-02-27 14:36:30 +0100

    update ASANA_PROJECT_ID to be correct type

--- scripts/eas-build-on-error.sh
+++ scripts/eas-build-on-error.sh
@@ -4,4 +4,4 @@ curl --request POST \
   --url 'https://app.asana.com/api/1.0/tasks' \
   --header 'Authorization: Bearer '"${ASANA_API_TOKEN}" \
   --header 'Content-Type: application/json' \
-  --data '{"data":{"name":"E2E Fail - '"${EAS_BUILD_PLATFORM}"' - '"${EAS_BUILD_GIT_COMMIT_HASH}"'","projects":['""${ASANA_PROJECT_ID}""'],"notes":"'"${EAS_BUILD_URL}"'","assignee":"me"}}'
+  --data '{"data":{"name":"E2E Fail - '"${EAS_BUILD_PLATFORM}"' - '"${EAS_BUILD_GIT_COMMIT_HASH}"'","projects":["'"${ASANA_PROJECT_ID}"'"],"notes":"'"${EAS_BUILD_URL}"'","assignee":"me"}}'
