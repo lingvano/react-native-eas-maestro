93373b31821f2b850d2a2ce5de50ceebaec42c09
Christof Thalmann <christof.thalmann@lingvano.com>
2023-02-27 15:53:32 +0100

    fix error vars for asana integration

--- scripts/eas-build-on-error.sh
+++ scripts/eas-build-on-error.sh
@@ -4,4 +4,4 @@ curl --request POST \
   --url 'https://app.asana.com/api/1.0/tasks' \
   --header 'Authorization: Bearer '"${ASANA_API_TOKEN}" \
   --header 'Content-Type: application/json' \
-  --data '{"data":{"name":"E2E Fail - '"${EAS_BUILD_PLATFORM}"' - '"${EAS_BUILD_GIT_COMMIT_HASH}"'","projects":["'"${ASANA_PROJECT_ID}"'"],"notes":"'"${EAS_BUILD_URL}"'","assignee":"me"}}'
+  --data '{"data":{"name":"E2E Fail - '"${EAS_BUILD_PLATFORM}"' - '"${EAS_BUILD_GIT_COMMIT_HASH}"'","projects":["'"${ASANA_PROJECT_ID}"'"],"notes":"https://expo.dev/accounts/react-native-eas-maestro/projects/react-native-eas-maestro/builds/'"${EAS_BUILD_ID}"'","assignee":"me"}}'
